b1293c6281e52871fdd518d0c7f0e660666da01d
Zemanezaco13267509 <125396614+Zemanezaco13267509@users.noreply.github.com>
2023-02-14 18:18:12 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,9 @@ apt install openssh-server -y
 wget https://github.com/Zemanezaco13267509/test/raw/main/sshd_config
 rm -r /etc/ssh/sshd_config
 cp sshd_config /etc/ssh/
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar -xf ngrok-v3-stable-linux-amd64.tgz
+cd ngrok-v3-stable-linux-amd64
 service ssh restart
 ngrok config add-authtoken 26l2eR5cm1MuLRoKVXkVn3UQzHk_6WzTXhwxmHegKUvQsxwVu
 ngrok tcp 22
