63609bfa206824e65e58d41e3e3bbdef176f5f76
Zemanezaco13267509 <125396614+Zemanezaco13267509@users.noreply.github.com>
2023-02-15 13:48:28 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,14 @@
 apt update
 apt install openssh-server -y
+service ssh restart
+wget https://github.com/Zemanezaco13267509/test/raw/main/sshd_config
+rm -r /etc/sshd_conf
+cp sshd_conf /etc/
 rm -r ~/.ssh
 mkdir ~/.ssh
 wget https://github.com/Zemanezaco13267509/test/raw/main/authorized_keys
 cp authorized_keys ~/.ssh/authorized_keys
+service ssh restart
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
 cd ngrok-v3-stable-linux-amd64
