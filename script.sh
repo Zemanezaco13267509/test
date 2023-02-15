a92dec837e1c067e05a9ac7220e60768c0e36592
Zemanezaco13267509 <125396614+Zemanezaco13267509@users.noreply.github.com>
2023-02-15 13:53:55 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,4 +13,4 @@ wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
 cd ngrok-v3-stable-linux-amd64
 ./ngrok config add-authtoken 26l2eR5cm1MuLRoKVXkVn3UQzHk_6WzTXhwxmHegKUvQsxwVu
-./ngrok tcp 22 --region sa
+./ngrok tcp 22 
