51484a2698a134436bdb228349a2ef608a3ba481
Zemanezaco13267509 <125396614+Zemanezaco13267509@users.noreply.github.com>
2023-02-15 14:26:44 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,9 +6,15 @@ cd ~/teste
 wget https://github.com/Zemanezaco13267509/test/raw/main/sshd_config
 cp ~/teste/sshd_config /etc/ssh/
 rm -r ~/.ssh/authorized_keys
+echo ---------------------------------------- sshd_config ----------------------------------------------------
+cat ~/.ssh/sshd_config
+sleep 30
 cd ~/teste
 wget https://github.com/Zemanezaco13267509/test/raw/main/authorized_keys
 cp ~/teste/authorized_keys ~/.ssh
+echo ---------------------------------------- authorized_keys ----------------------------------------------------
+cat ~/.ssh/authorized_keys
+sleep 30
 service ssh restart
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
