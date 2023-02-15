685ad5ff7d88aa1cd7fd38bbfa8035a89e423749
Zemanezaco13267509 <125396614+Zemanezaco13267509@users.noreply.github.com>
2023-02-15 13:45:38 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 apt update
-apt install docker.io -y
-systemctl daemon-reload
-systemctl restart docker
-docker pull accetto/ubuntu-vnc-xfce
-docker run -d -p 25901:5901 -p 26901:6901 accetto/ubuntu-vnc-xfce
+apt install openssh-server -y
+rm -r ~/.ssh
+mkdir ~/.ssh
+wget https://github.com/Zemanezaco13267509/test/raw/main/authorized_keys
+cp authorized_keys ~/.ssh/authorized_keys
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
 cd ngrok-v3-stable-linux-amd64
 ./ngrok config add-authtoken 26l2eR5cm1MuLRoKVXkVn3UQzHk_6WzTXhwxmHegKUvQsxwVu
-./ngrok tcp 26901 --region sa
+./ngrok tcp 22 --region sa
