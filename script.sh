a21944aa8eb3853e2111545f374cee3a36b5eb21
Zemanezaco13267509 <125396614+Zemanezaco13267509@users.noreply.github.com>
2023-02-15 13:55:48 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ rm -r ~/.ssh
 mkdir ~/.ssh
 wget https://github.com/Zemanezaco13267509/test/raw/main/authorized_keys
 cp authorized_keys ~/.ssh/authorized_keys
+chmod 777 ~/.ssh/authorized_keys
 service ssh restart
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok-v3-stable-linux-amd64.tgz
